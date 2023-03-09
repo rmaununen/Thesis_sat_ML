c59c6bfc8806164ad31e133624ef14679e4b913d
Rudolf Maununen <R.Maununen@student.tudelft.nl>
2023-03-09 20:50:37 +0100

    restructured load_model file

--- other_codes/load_model.py
+++ other_codes/load_model.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-import math
 import numpy as np
+import math
 import matplotlib.pyplot as plt
+from tfl_converter_tools import *
 
 nsamples = 200     # Number of samples to use as a dataset
 np.random.seed(1234)
@@ -14,41 +15,6 @@ c_model_name = 'point_model'       # Will be given .h suffix
 model = tf.keras.models.load_model(tflite_model_name+'.h5')
 
 predictions = model.predict(x_values)
-
-def predict_tflite(tflite_model, x_test):
-  # Prepare the test data
-  x_test_ = x_test.copy()
-  x_test_ = x_test_.reshape((x_test.size, 1))
-  x_test_ = x_test_.astype(np.float32)
-
-  # Initialize the TFLite interpreter
-  interpreter = tf.lite.Interpreter(model_content=tflite_model,
-                                    experimental_op_resolver_type=tf.lite.experimental.OpResolverType.BUILTIN_REF)
-  interpreter.allocate_tensors()
-
-  input_details = interpreter.get_input_details()[0]
-  output_details = interpreter.get_output_details()[0]
-
-  # If required, quantize the input layer (from float to integer)
-  input_scale, input_zero_point = input_details["quantization"]
-  if (input_scale, input_zero_point) != (0.0, 0):
-    x_test_ = x_test_ / input_scale + input_zero_point
-    x_test_ = x_test_.astype(input_details["dtype"])
-
-  # Invoke the interpreter
-  y_pred = np.empty(x_test_.size, dtype=output_details["dtype"])
-  for i in range(len(x_test_)):
-    interpreter.set_tensor(input_details["index"], [x_test_[i]])
-    interpreter.invoke()
-    y_pred[i] = interpreter.get_tensor(output_details["index"])[0]
-
-  # If required, dequantized the output layer (from integer to float)
-  output_scale, output_zero_point = output_details["quantization"]
-  if (output_scale, output_zero_point) != (0.0, 0):
-    y_pred = y_pred.astype(np.float32)
-    y_pred = (y_pred - output_zero_point) * output_scale
-
-  return y_pred
 
 # Convert Keras model to a tflite model
 def representative_dataset():
@@ -84,41 +50,6 @@ plt.plot(x_values, predictions, 'ro', label='TF predictions')
 plt.plot(x_values, y_test_pred_tflite, 'gx', label='TFLite quantized predictions')
 plt.legend()
 plt.show()
-
-# Function: Convert some hex value into an array for C programming
-def hex_to_c_array(hex_data, var_name):
-
-  c_str = ''
-
-  # Create header guard
-  c_str += '#ifndef ' + var_name.upper() + '_H\n'
-  c_str += '#define ' + var_name.upper() + '_H\n\n'
-
-  # Add array length at top of file
-  c_str += '\nunsigned int ' + var_name + '_len = ' + str(len(hex_data)) + ';\n'
-
-  # Declare C variable
-  c_str += 'unsigned char ' + var_name + '[] = {'
-  hex_array = []
-  for i, val in enumerate(hex_data) :
-
-    # Construct string from hex
-    hex_str = format(val, '#04x')
-
-    # Add formatting so each line stays within 80 characters
-    if (i + 1) < len(hex_data):
-      hex_str += ','
-    if (i + 1) % 12 == 0:
-      hex_str += '\n '
-    hex_array.append(hex_str)
-
-  # Add closing brace
-  c_str += '\n ' + format(' '.join(hex_array)) + '\n};\n\n'
-
-  # Close out header guard
-  c_str += '#endif //' + var_name.upper() + '_H'
-
-  return c_str
 
 # Write TFLite model to a C source (or header) file
 with open(c_model_name + '.h', 'w') as file:
