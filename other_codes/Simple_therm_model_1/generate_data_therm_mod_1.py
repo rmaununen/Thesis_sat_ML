6512df299588b39b74272611e70b738c8b63f19e
Rudolf Maununen <R.Maununen@student.tudelft.nl>
2023-03-10 22:47:15 +0100

    added variation of thermal profiles

--- other_codes/Simple_therm_model_1/generate_data_therm_mod_1.py
+++ other_codes/Simple_therm_model_1/generate_data_therm_mod_1.py
@@ -1,20 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from random import randrange
 
 # Define variables
+n1 = randrange(90, 110)/100
+n2 = randrange(90, 110)/100
+n3 = randrange(90, 110)/100
+n4 = randrange(90, 110)/100
+n5 = randrange(90, 110)/100
+n6 = randrange(95, 105)/100
+n7 = randrange(95, 105)/100
+n8 = randrange(95, 105)/100
+n9 = randrange(95, 105)/100
+n10 = randrange(95, 105)/100
 
-c = 7
-
+c = randrange(680, 720)/100
 
 # Define x values
 x = np.linspace(0, 2*np.pi, 1000)
 
 # Define y values for the growing exponential line
-y1 = -10 + c*(6 - np.exp(-1 * (0.9*x-1.79)))
+y1 = -10*n5 + c*(6*n4 - np.exp(-1*n3 * (n1*0.9*x-(1.79*n2))))
 
 # Define y values for the decaying exponential line
 
-y2 = -10 + c * (-1 + np.exp(-0.6 * (x - 2*np.pi)))
+y2 = -10*n10 + c * (-1*n9 + np.exp(-0.6*n8 * (x*n7 - 2*np.pi*n6)))
 
 y3 = np.concatenate((y1[:500], y2[-500:]))
 # Combine the two lines into one plot
@@ -23,7 +33,7 @@ plt.plot(x, y3, 'r-', label='Growing')
 
 # Set the plot limits and labels
 plt.xlim([0, 2*np.pi])
-plt.ylim([-11, 33])
+plt.ylim([-11, 58])
 plt.xlabel('theta rad')
 plt.ylabel('temp deg C')
 plt.title('S/C Temp')
